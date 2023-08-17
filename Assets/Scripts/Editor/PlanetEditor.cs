7f30a6880ae77c43f11fa9a44f2772d4c27b6bc9
quartzar <joey.dorn7@gmail.com>
2023-08-17 14:24:01 +0100

    Initial commit

---
+++ Assets/Scripts/Editor/PlanetEditor.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(Planet))]
+public class PlanetEditor : Editor
+{
+    Planet planet;
+    Editor shapeEditor;
+    Editor colourEditor;
+
+    public override void OnInspectorGUI()
+    {
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            base.OnInspectorGUI();
+            if (check.changed)
+            {
+                planet.GeneratePlanet();
+            }
+        }
+        // Button for manual mesh regeneration
+        if (GUILayout.Button("Generate Planet"))
+        {
+            planet.GeneratePlanet();
+        }
+        
+        DrawSettingsEditor(planet.shapeSettings, planet.OnShapeSettingsUpdated, ref planet.shapeSettingsFoldout, ref shapeEditor);
+        DrawSettingsEditor(planet.colourSettings, planet.OnColourSettingsUpdated, ref planet.colourSettingsFoldout, ref colourEditor);
+    }
+
+    void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated, ref bool foldout, ref Editor editor)
+    {
+        if (settings != null)
+        {
+            foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                if (foldout)
+                {
+                    // Stores the editor for the settings object, and creates one if it doesn't exist
+                    CreateCachedEditor(settings, null, ref editor);
+                    editor.OnInspectorGUI();
+                    
+                    if (check.changed)
+                    {
+                        if (onSettingsUpdated != null)
+                        {
+                            onSettingsUpdated();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        planet = (Planet) target;
+    }
+}
+
---
+++ Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TerrainFace
+{
+    private ShapeGenerator _shapeGenerator;
+    private Mesh _mesh;
+    private int _resolution;
+    private Vector3 _localUp;
+    private Vector3 _axisA;
+    private Vector3 _axisB;
+    
+    public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this._shapeGenerator = shapeGenerator;
+        this._mesh = mesh;
+        this._resolution = resolution;
+        this._localUp = localUp;
+        
+        _axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        _axisB = Vector3.Cross(localUp, _axisA);
+    }
+
+    public void ConstructMesh()
+    {
+        Vector3[] vertices = new Vector3[_resolution * _resolution];
+        int[] triangles = new int[(_resolution - 1) * (_resolution - 1) * 6];
+        Vector2[] uv = (_mesh.uv.Length == vertices.Length) ? _mesh.uv : new Vector2[vertices.Length];
+        int triIndex = 0;
+        
+        for (int y = 0; y < _resolution; y++)
+        {
+            for (int x = 0; x < _resolution; x++)
+            {
+                int i = x + y * _resolution;
+                Vector2 percent = new Vector2(x, y) / (_resolution - 1);
+                Vector3 pointOnCube = _localUp + (percent.x - 0.5f) * 2 * _axisA + (percent.y - 0.5f) * 2 * _axisB;
+                Vector3 pointOnUnitSphere = pointOnCube.normalized;
+                float unscaledElevation = _shapeGenerator.CalculateUnscaledElevation(pointOnUnitSphere);
+                vertices[i] = pointOnUnitSphere * _shapeGenerator.GetScaledElevation(unscaledElevation);
+                uv[i].y = unscaledElevation;
+                
+                if (x != _resolution - 1 && y != _resolution - 1)
+                {
+                    triangles[triIndex + 0] = i;
+                    triangles[triIndex + 1] = i + _resolution + 1;
+                    triangles[triIndex + 2] = i + _resolution;
+                    
+                    triangles[triIndex + 3] = i;
+                    triangles[triIndex + 4] = i + 1;
+                    triangles[triIndex + 5] = i + _resolution + 1;
+                    triIndex += 6;
+                }
+            }
+        }
+        _mesh.Clear();
+        _mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32; // This is needed for meshes with more than 65535 [256*256] vertices
+        _mesh.vertices = vertices;
+        _mesh.triangles = triangles;
+        _mesh.RecalculateNormals();
+        _mesh.uv = uv;
+    }
+    
+    public void UpdateUVs(ColourGenerator colourGenerator)
+    {
+        Vector2[] uv = _mesh.uv;
+        
+        for (int y = 0; y < _resolution; y++)
+        {
+            for (int x = 0; x < _resolution; x++)
+            {
+                int i = x + y * _resolution;
+                Vector2 percent = new Vector2(x, y) / (_resolution - 1);
+                Vector3 pointOnUnitCube = _localUp + (percent.x - 0.5f) * 2 * _axisA + (percent.y - 0.5f) * 2 * _axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                
+                uv[i].x = colourGenerator.BiomePercentFromPoint(pointOnUnitSphere);
+            }
+        }
+        _mesh.uv = uv;
+    }
+}
