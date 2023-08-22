af8aa3b3c9f04c72cd241e52aaddb276fa1cbd6b
quartzar <joey.dorn7@gmail.com>
2023-08-22 11:39:35 +0100

    Archive NoiseSettings

--- Assets/Scripts/NoiseSettings.cs
+++ Assets/Scripts/NoiseSettings.cs
@@ -1,37 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class NoiseSettings {
-
-    public enum FilterType { Simple, Rigid };
-    public FilterType filterType;
-
-    [ConditionalHide("filterType", 0)]
-    public SimpleNoiseSettings simpleNoiseSettings;
-    [ConditionalHide("filterType", 1)]
-    public RigidNoiseSettings rigidNoiseSettings;
-
-    [System.Serializable]
-    public class SimpleNoiseSettings
-    {
-        [Range(1, 8)]
-        public int numLayers = 1;
-        // [Range(0, 2)]
-        public float scale = 1;
-        public float persistence = 0.5f;
-        public float lacunarity = 2;
-        // [Range(0, 1)]
-        public float multiplier = 1;
-    }
-
-    [System.Serializable]
-    public class RigidNoiseSettings : SimpleNoiseSettings
-    {
-        public float weightMultiplier = 0.8f;
-    }
- 
-
-
-}
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// [System.Serializable]
+// public class NoiseSettings {
+//
+//     public enum FilterType { Simple, Rigid };
+//     public FilterType filterType;
+//
+//     [ConditionalHide("filterType", 0)]
+//     public SimpleNoiseSettings simpleNoiseSettings;
+//     [ConditionalHide("filterType", 1)]
+//     public RigidNoiseSettings rigidNoiseSettings;
+//
+//     [System.Serializable]
+//     public class SimpleNoiseSettings
+//     {
+//         [Range(1, 8)]
+//         public int numLayers = 1;
+//         [Range(0, 0.75f)]
+//         public float scale = 0.2f;
+//         public float persistence = 0.5f;
+//         public float lacunarity = 2;
+//         // [Range(0, 1)]
+//         public float multiplier = 1;
+//         public float verticalShift = 0;
+//     }
+//
+//     [System.Serializable]
+//     public class RigidNoiseSettings : SimpleNoiseSettings
+//     {
+//         public float weightMultiplier = 0.8f;
+//     }
+//
+// }
