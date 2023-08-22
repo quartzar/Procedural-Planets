b54df8fe9ce15c3849d8b6f7b6b35d25e85c4831
quartzar <joey.dorn7@gmail.com>
2023-08-22 11:40:18 +0100

    Cleaning up TerrainFace

--- Assets/Scripts/TerrainFace.cs
+++ Assets/Scripts/TerrainFace.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using NoiseSettings;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class TerrainFace
 {
-    private readonly NoiseSettings.SimpleNoiseSettings _noiseSettings;
-    private readonly ShapeSettings _shapeSettings;
+    // private readonly SimpleNoiseSettings _continentSettings;
+    // private readonly SimpleNoiseSettings _maskSettings;
+    // private readonly RidgeNoiseSettings _ridgeSettings;
+    // private readonly ShapeSettings _shapeSettings;
+    private readonly CelestialBodySettings _body;
     private readonly Mesh _mesh;
     private readonly int _resolution;
+    private readonly float _radius;
     private readonly Vector3 _localUp;
     private readonly Vector3 _axisA;
     private readonly Vector3 _axisB;
-    
-    public TerrainFace(NoiseSettings.SimpleNoiseSettings noiseSettings, ShapeSettings shapeSettings, Mesh mesh, int resolution, Vector3 localUp)
+    private static readonly int Heights = Shader.PropertyToID("heights");
+    private static readonly int Vertices = Shader.PropertyToID("vertices");
+
+    public TerrainFace(CelestialBodySettings body, Mesh mesh, int resolution, float radius, Vector3 localUp)
     {
-        this._noiseSettings = noiseSettings;
-        this._shapeSettings = shapeSettings;
+        // this._continentSettings = continentSettingsSettings;
+        // this._maskSettings = maskSettings;
+        // this._ridgeSettings = ridgeNoiseSettings;
+        // this._shapeSettings = shapeSettings;
+        this._body = body;
         this._mesh = mesh;
         this._resolution = resolution;
+        this._radius = radius;
         this._localUp = localUp;
         
         _axisA = new Vector3(localUp.y, localUp.z, localUp.x);
@@ -39,7 +50,7 @@ public class TerrainFace
                 int i = x + y * _resolution;
                 Vector2 percent = new Vector2(x, y) / (_resolution - 1);
                 Vector3 pointOnCube = _localUp + (percent.x - 0.5f) * 2 * _axisA + (percent.y - 0.5f) * 2 * _axisB;
-                Vector3 pointOnUnitSphere = pointOnCube.normalized * _shapeSettings.planetRadius;
+                Vector3 pointOnUnitSphere = pointOnCube.normalized * _radius;
                 vertices[i] = pointOnUnitSphere;
                 
                 if (x != _resolution - 1 && y != _resolution - 1)
@@ -64,45 +75,109 @@ public class TerrainFace
     }
     
     
-    public void AddNoiseWithComputeShader(ComputeShader noiseShader)
+    public void NoiseShader()
     {
-        // 1. Get the kernel id
-        int kernelHandle = noiseShader.FindKernel("CSMain");
-        
-        // 2. Create a compute buffer to hold the vertices
         ComputeBuffer vertexBuffer = new ComputeBuffer(_mesh.vertexCount, 3 * sizeof(float)); // 3 for x, y, z
-
-        // 3. Set the buffer data from the mesh vertices
-        vertexBuffer.SetData(_mesh.vertices);
-
-        // 4. Set the buffer to the compute shader
-        noiseShader.SetBuffer(kernelHandle, "VertexBuffer", vertexBuffer);
-        noiseShader.SetInt(Shader.PropertyToID("numVertices"), _mesh.vertexCount);
-        noiseShader.SetFloat(Shader.PropertyToID("numLayers"), _noiseSettings.numLayers);
-        noiseShader.SetFloat(Shader.PropertyToID("scale"), _noiseSettings.scale);
-        noiseShader.SetFloat(Shader.PropertyToID("persistence"), _noiseSettings.persistence);
-        noiseShader.SetFloat(Shader.PropertyToID("lacunarity"), _noiseSettings.lacunarity);
-        noiseShader.SetFloat(Shader.PropertyToID("multiplier"), _noiseSettings.multiplier);
-        noiseShader.SetFloat(Shader.PropertyToID("oceanDepthMultiplier"), _shapeSettings.oceanDepthMultiplier);
-        noiseShader.SetFloat(Shader.PropertyToID("oceanFloorDepth"), _shapeSettings.oceanFloorDepth);
-        noiseShader.SetFloat(Shader.PropertyToID("oceanFloorSmoothing"), _shapeSettings.oceanFloorSmoothing);
+        float[] heights = _body.shape.ComputeHeights(vertexBuffer);
         
-
-        // 5. Dispatch the compute shader
-        // int threadGroups = Mathf.CeilToInt(_mesh.vertexCount / 1.0f); // Assuming each thread handles 1 vertex
-        int threadsPerGroup = 512;
-        int threadGroups = Mathf.CeilToInt(_mesh.vertexCount / (float)threadsPerGroup);
-        noiseShader.Dispatch(kernelHandle, threadGroups, 1, 1);
-
-        // 6. Retrieve the data from the buffer
-        Vector3[] modifiedVertices = new Vector3[_mesh.vertexCount];
-        vertexBuffer.GetData(modifiedVertices);
-        _mesh.vertices = modifiedVertices;  
-        _mesh.RecalculateNormals(); 
-
-        // 7. Cleanup  
-        vertexBuffer.Release();
+        // Modify the mesh vertices using the heights
+        Vector3[] modifiedVertices = _mesh.vertices;
+        for (int i = 0; i < modifiedVertices.Length; i++)
+        {
+            float height = heights[i];
+            modifiedVertices[i] *= height; 
+            Planet.elevationMinMax.AddValue(height);
+        }
+        _mesh.vertices = modifiedVertices;
+        _mesh.RecalculateNormals();
     }
+    
+    // public void AddNoiseWithComputeShader(ComputeShader noiseShader)
+    // {
+    //     // 1. Get the kernel id
+    //     int kernelHandle = noiseShader.FindKernel("CSMain");
+    //
+    //     // 2. Create a compute buffer to hold the vertices
+    //     ComputeBuffer vertexBuffer = new ComputeBuffer(_mesh.vertexCount, 3 * sizeof(float)); // 3 for x, y, z
+    //
+    //     // 3. Create a compute buffer to hold the heights
+    //     ComputeBuffer heightBuffer = new ComputeBuffer(_mesh.vertexCount, sizeof(float));
+    //
+    //     // 4. Set the buffer data from the mesh vertices
+    //     vertexBuffer.SetData(_mesh.vertices);
+    //
+    //     // 5. Set the buffer to the compute shader
+    //     noiseShader.SetBuffer(kernelHandle, Vertices, vertexBuffer);
+    //     noiseShader.SetBuffer(kernelHandle, Heights, heightBuffer);
+    //     noiseShader.SetInt(Shader.PropertyToID("numVertices"), _mesh.vertexCount);
+    //     
+    //     noiseShader.SetFloat(Shader.PropertyToID("numLayers"), _continentSettings.numLayers);
+    //     noiseShader.SetFloat(Shader.PropertyToID("scale"), _continentSettings.scale/10f);
+    //     noiseShader.SetFloat(Shader.PropertyToID("persistence"), _continentSettings.persistence);
+    //     noiseShader.SetFloat(Shader.PropertyToID("lacunarity"), _continentSettings.lacunarity);
+    //     noiseShader.SetFloat(Shader.PropertyToID("multiplier"), _continentSettings.elevation);
+    //     noiseShader.SetFloat(Shader.PropertyToID("oceanDepthMultiplier"), _shapeSettings.oceanDepthMultiplier);
+    //     noiseShader.SetFloat(Shader.PropertyToID("oceanFloorDepth"), _shapeSettings.oceanFloorDepth);
+    //     noiseShader.SetFloat(Shader.PropertyToID("oceanFloorSmoothing"), _shapeSettings.oceanFloorSmoothing);
+    //     noiseShader.SetFloat(Shader.PropertyToID("mountainBlend"), _shapeSettings.mountainBlend);
+    //     
+    //     float[] maskParams = {
+    //         // [0]
+    //         _maskSettings.scale/10f,
+    //         _maskSettings.persistence,
+    //         _maskSettings.lacunarity,
+    //         _maskSettings.elevation,
+    //         // [1]
+    //         _maskSettings.numLayers,
+    //         _maskSettings.verticalShift
+    //     };
+    //     noiseShader.SetFloats(Shader.PropertyToID("noiseParams_mask"), maskParams);
+    //     
+    //     float[] ridgeParams = {
+    //         // [0]
+    //         _ridgeSettings.offset.x,
+    //         _ridgeSettings.offset.y,
+    //         _ridgeSettings.offset.z,
+    //         _ridgeSettings.numLayers,
+    //         // [1]
+    //         _ridgeSettings.persistence,
+    //         _ridgeSettings.lacunarity,
+    //         _ridgeSettings.scale,
+    //         _ridgeSettings.elevation,
+    //         // [2]
+    //         _ridgeSettings.power,
+    //         _ridgeSettings.gain,
+    //         _ridgeSettings.verticalShift,
+    //         _ridgeSettings.peakSmoothing
+    //     };
+    //     noiseShader.SetFloats(Shader.PropertyToID("noiseParams_mountains"), ridgeParams);
+    //
+    //     // 6. Dispatch the compute shader
+    //     int threadsPerGroup = 512;
+    //     int threadGroups = Mathf.CeilToInt(_mesh.vertexCount / (float)threadsPerGroup);
+    //     noiseShader.Dispatch(kernelHandle, threadGroups, 1, 1);
+    //     
+    //     // 7. Retrieve the heights from the buffer
+    //     float[] heights = new float[_mesh.vertexCount];
+    //     heightBuffer.GetData(heights);
+    //     
+    //     
+    //
+    //     // 8. Modify the mesh vertices using the heights
+    //     Vector3[] modifiedVertices = _mesh.vertices;
+    //     for (int i = 0; i < modifiedVertices.Length; i++)
+    //     {
+    //         float height = heights[i];
+    //         modifiedVertices[i] *= height; 
+    //         Planet.elevationMinMax.AddValue(height);
+    //     }
+    //     _mesh.vertices = modifiedVertices;
+    //     _mesh.RecalculateNormals();
+    //
+    //     // 9. Cleanup
+    //     vertexBuffer.Release();
+    //     heightBuffer.Release();
+    // }
     
     
     
